4623b4f1917b8bcc06bb260c1da239582f80187d
ahkr <ao.hk215@gmail.com>
2016-07-13 17:19:51 +0900

    Ctrl+Cで処理を中断された場合のエラーを処理する

--- lib/main.rb
+++ lib/main.rb
@@ -1,13 +1,14 @@
 require 'readline'
 require_relative './calculator'
 
-input = Readline.readline("1から1000の間で任意の数字を入力してください\n> ")
-
 begin
+  input = Readline.readline("1から1000の間で任意の数字を入力してください\n> ")
   calculator = Calculator.new
   calculator.remove_number(input)
   result = calculator.extract_removed_number
   puts "あなたが入力した数字は#{result}です"
+rescue Interrupt
+  system('stty', `stty -g`)
 rescue Calculator::InputValueOutOfRangeError => e
   puts "エラー内容: #{e.message}\n入力結果: #{input}"
 rescue => e
