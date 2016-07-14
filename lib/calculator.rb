13133135191b10466167ad8b58908930131d96fb
umeyuki <umeyuki1326+github@gmail.com>
2016-07-14 10:54:46 +0900

    Add a instance variable total and a method extract_removed_number_with_inject

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,23 +1,28 @@
 class Calculator
+  MAX_NUMBER = 1000
   THOUSAND_FROM_ONE = [*1..1000].shuffle
 
-  attr_accessor :dup_numbers
+  attr_accessor :dup_numbers, :total
 
   def initialize
     @dup_numbers = THOUSAND_FROM_ONE.dup
+    @total = THOUSAND_FROM_ONE.inject(:+)
   end
 
   def remove_number(num)
-    fail(InputValueOutOfRangeError, '1から1000の間で数字を入力してください') unless num =~ /^[1-9]\d{0,2}$|1000/
+    fail(InputValueOutOfRangeError, '1から1000の間で数字を入力してください') unless num.between?(1, MAX_NUMBER)
     dup_numbers.delete(num.to_i)
   end
 
-  def extract_removed_number
+  def extract_removed_number_with_loop
     THOUSAND_FROM_ONE.each_with_index do |number, i|
       return number if number != dup_numbers[i]
     end
   end
 
+  def extract_removed_number_with_inject
+    total - dup_numbers.inject(:+)
+  end
+
   class InputValueOutOfRangeError < StandardError; end
 end
-
